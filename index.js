5c04bad418803aff35795553985adda9bcc4fcf8
farste <sfargali@gmail.com>
2019-03-20 15:29:22 -0400

    made package.json, added server.listen

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const server = express();
 const cors = require("cors");
 server.use(cors());
 server.use(express.json());
+
+const port = 3300;
+server.listen(port, function() {
+  console.log(`\n Web API Listening on localhost:${port}\n`);
+});
