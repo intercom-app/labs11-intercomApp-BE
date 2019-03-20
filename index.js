d45d3a29c7aa229cf109617e29c01f0a9011a6e1
farste <sfargali@gmail.com>
2019-03-20 15:34:18 -0400

    Backend now prints hello world when accessed

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const cors = require("cors");
 server.use(cors());
 server.use(express.json());
 
+server.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n Web API Listening on localhost:${port}\n`);
