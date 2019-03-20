dd44e33f726eaf06ab6e025e2915fdef820f5211
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-03-20 18:18:38 -0400

    Added additional dependencies for testing and DB. Updated knexfile.js to include testing and production configs. Removed duplicate unused sqlite db file with mispelling.

--- knexfile.js
+++ knexfile.js
@@ -1,25 +1,51 @@
-// const localPgConnection = {
-//     host: 'localhost',
-//     database: 'app',
-//     user: 'admin',
-//     password: 'password'
-//   }
-//   const prodDbConnection = process.env.DATABASE_URL || localPgConnection;
+const localPgConnection = {
+    host: 'localhost',
+    database: 'app',
+    user: 'admin',
+    password: 'password'
+  }
+  const prodDbConnection = process.env.DATABASE_URL || localPgConnection;
 
 
 module.exports = {
 
-    development: {
-      client: 'sqlite3',
-      connection: {
-        filename: './data/intercomAppDevelopmentDB.sqlite3'
-      },
-      useNullAsDefault: true,
-          migrations: { 
-        directory: './data/migrations'
-      },
-          seeds: { 
-        directory: './data/seeds'
-      }
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/intercomAppDevelopmentDB.sqlite3'
+    },
+    useNullAsDefault: true,
+    migrations: { 
+      directory: './data/migrations'
+    },
+    seeds: { 
+      directory: './data/seeds'
     }
-  };
+  },
+
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/intercomAppTestDB.sqlite3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+
+  production: {
+    client: 'pg',
+    connection: prodDbConnection,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  }
+
+};
