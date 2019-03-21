c3a79787f81e2a4d4619f041a2f84cefbfe502e5
Adrian Adames <adrianadames@gmail.com>
2019-03-21 17:15:43 -0400

    Changed name of initial users table to teamMates.

--- data/migrations/20190321161651_usersTable.js
+++ data/migrations/20190321161651_usersTable.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex, Promise) {
     // create the users tables
-    return knex.schema.createTable('usersTable', function(tbl) {
+    return knex.schema.createTable('users', function(tbl) {
         // user id (primary key)
         tbl.increments(); // creates an id (if you don't pass anything here the default name of the column will be 'id'), makes it integer, makes it autoincrement
 
