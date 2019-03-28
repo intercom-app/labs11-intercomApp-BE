44e15d26daf1461e86455316173d26efad665845
farste <sfargali@gmail.com>
2019-03-28 15:26:10 -0400

    pre-merge changes to server.js

--- api/server.js
+++ api/server.js
@@ -14,11 +14,6 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
-
-const VoiceResponse = require('twilio').twiml.VoiceResponse;
-const urlencoded = require('body-parser').urlencoded;
-
-
 server.use('/api/team', teamRouter);
 server.use('/api/users', usersRouter);
 server.use('/api/groups', groupRouter);
@@ -32,20 +27,5 @@ server.post('/test', (req, res) => {
     console.log('req', req.body)    
     res.send('Hello World!');
 });
-
-server.post('/api/voice', (request, response) => {
-    // Use the Twilio Node.js SDK to build an XML response
-    const twiml = new VoiceResponse();
-  
-    // Start with a <Dial> verb
-    const dial = twiml.dial();
-      dial.conference('My conference', {
-        startConferenceOnEnter: true,
-      });
-  
-    // Render the response as XML in reply to the webhook request
-    response.type('text/xml');
-    response.send(twiml.toString());
-  });
 
 module.exports = server;
