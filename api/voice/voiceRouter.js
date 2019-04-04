7824aa72b1680484f94562199ded35fbedd0075a
farste <sfargali@gmail.com>
2019-04-04 14:04:00 -0400

    Hooked up voice to iOS, device to device calls now working.

--- api/voice/voiceRouter.js
+++ api/voice/voiceRouter.js
@@ -1,15 +1,48 @@
 const Router = require('express').Router;
 
-const {tokenGenerator, voiceResponse} = require('./voiceHandler');
+const voiceHandler = require('./voiceHandler');
 
 const router = new Router();
+var bodyParser = require('body-parser')
+router.use(bodyParser())
+/**
+ * Generate a Capability Token for a Twilio Client user - it generates a random
+ * username for the client requesting a token.
+ */
+router.get('/accessToken', (req, res) => {
+  res.send(voiceHandler.tokenGenerator(req));
+});
 
-router.get('/token', (req, res) => {
-  res.send(tokenGenerator());
+router.get('/incoming', function(req, res) {
+  res.send(voiceHandler.incoming());
+});
+
+router.post('/incoming', function(req, res) {
+  res.send(voiceHandler.incoming());
+});
+
+router.get('/ntstoken', (req, res) => {
+  res.send(voiceHandler.generateNTSToken());
+});
+
+router.get('/makeCall', (req, res) => {
+  res.send(voiceHandler.makeCall(req, res));
+});
+
+router.post('/makeCall', (req, res) => {
+  res.send(voiceHandler.makeCall(req, res));
+});
+
+router.get('/placeCall', (req, res) => {
+  res.send(voiceHandler.placeCall(req, res));
+});
+
+router.post('/placeCall', (req, res) => {
+  res.send(voiceHandler.placeCall(req, res));
 });
 
 router.post('/', (req, res) => {
-  res.send(voiceResponse(req.body.To));
+  res.send(voiceHandler.voiceResponse(req.body.To));
 });
 
 module.exports = router;
