8d1dcabd5a6ec1cef75e86a00057dfc10108e760
Adrian Adames <adrianadames@gmail.com>
2019-04-15 23:59:10 -0400

    Minor edit.

--- api/billing/billingRouter.js
+++ api/billing/billingRouter.js
@@ -179,9 +179,9 @@ router.post('/groupTwilioCharges', async(req,res) => {
     }
 
     res.status(200).json({'sumOfGroupTwilioCharges':sumOfGroupTwilioCharges});
-
   } catch(err) {
     console.log(err)
+    res.status(500).json(err);
   }
 })
 
